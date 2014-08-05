720ea8535fe9d7ee3b974e5e8955c74a9fb19c8d
otknoy <otknoy@gmail.com>
2014-08-05 16:36:04 +0900

    Refactoring

--- parse.py
+++ parse.py
@@ -1,52 +1,69 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 import re
-import nlp
+import unicodedata
+import json
 
-def loadFile(filename):
-    f = open(filename)
-    text = nlp.normalize(f.read().decode('utf-8'))
-    f.close()
+def split(s):
+    s = s.replace(u'＼ＩＤ＼', u'\n＼ＩＤ＼')
+    return s.split('\n\n')[1:]
 
-    texts = split(text)
-    docs = []
-    for text in texts:
-        doc = parse(text)
-        docs.append(doc)
+def normalize(s):
+    return unicodedata.normalize('NFKC', s)
 
-    return docs
-
-def split(text):
-    text = text.replace('\\ID\\', '\n\n\\ID\\')
-    return text.split('\n\n')[1:]
-
-def parse(text):
-    doc = {}
-    for line in text.split('\n'):
-        match = re.search(r'^\\(.+)\\(.+)', line)
+def format_to_csv(s):
+    temp = []
+    for l in s.split('\n'):
+        match = re.search(r'^\\(.+)\\(.+)', l)
         if not match:
             continue
-        key = match.group(1)
-        value = match.group(2)
 
-        if not doc.has_key(key):
-            doc[key] = []
-        doc[key].append(value)
-        
+        k = match.group(1)
+        v = match.group(2).strip()
+        temp.append(k + ',' + v)
+    return '\n'.join(temp)
+
+def parse(s):
+    doc = {}
+    for l in s.split('\n'):
+        match = re.search(r'^\\(.+)\\(.+)', l)
+        if not match:
+            continue
+        k = match.group(1)
+        v = match.group(2)
+        if not doc.has_key(k):
+            doc[k] = []
+        doc[k].append(v)
     return doc
+
+def format(d):
+    # d = json.dumps(d, indent=True, sort_keys=True)
+    s = ''
+    for k, v in d.items():
+        for e in v:
+            s += '%s,%s\n' % (k, e)
+    return s.encode('utf-8')
+
+def output_to_file(doc):
+    out_dir = 'output'
+    # fname = doc['ID'][0] + '_' + doc['T1'][0] + '.txt'
+    fname = doc['ID'][0] + '.txt'    
+    filename = os.path.join(out_dir, fname)
+
+    f = open(filename, 'w')
+    f.write(format(d))
+    f.close()
 
 
 if __name__ == '__main__':
-    filename = 'data/mai2009_utf8.txt'
+    import sys
+    filename = sys.argv[1]
 
-    docs = loadFile(filename)
+    text = open(filename).read().decode('utf-8')
+    articles = split(text)
 
-    out_dir = 'output/'
-    for doc in docs:
-        filename = out_dir + doc['ID'][0] + '.txt'
-        article = '\n'.join(doc['T2'])
-
-        f = open(filename, 'w')
-        f.write(article.encode('utf-8'))
-        f.close()
-    
+    for article in articles:
+        a = normalize(article)
+        d = parse(a)
+        output_to_file(d)
